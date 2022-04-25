dfdc3b6b084d1ea9d08a6692d2cc7e92915de760
ctyQwQ <cty20000216@gmail.com>
2022-04-25 11:41:55 +0800

    ok

---
+++ B10756051_0425/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+
+namespace B10756051_0425
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TcpListener Server;
+        Socket Client;
+        Thread Th_Svr;
+        Thread Th_Clt;
+        Hashtable HT = new Hashtable();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Th_Svr = new Thread(ServerSub);
+            Th_Svr.IsBackground = true;
+            Th_Svr.Start();
+            button1.Enabled = false;
+        }
+
+        private void ServerSub()
+        {
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+
+            Server = new TcpListener(EP);
+            Server.Start(100);
+            while(true)
+            {
+                Client = Server.AcceptSocket();
+                Th_Clt = new Thread(Listen);
+                Th_Clt.IsBackground = true;
+                Th_Clt.Start();
+            }
+        }
+
+        private void Listen()
+        {
+            Socket Sck = Client;
+            Thread TH = Th_Clt;
+            while(true)
+            {
+                try
+                {
+                    byte[] B = new byte[1023];
+                    int inLen = Sck.Receive(B);
+                    string Msg = Encoding.Default.GetString(B, 0, inLen);
+
+                    string Cmd = Msg.Substring(0, 1);
+                    string Str = Msg.Substring(1);
+                    switch(Cmd)
+                    {
+                        case "0":
+                            HT.Add(Str, Sck);
+                            listBox1.Items.Add(Str);
+                            break;
+                        case "9":
+                            HT.Remove(Str);
+                            listBox1.Items.Remove(Str);
+                            TH.Abort();
+                            break;
+                    }
+                }
+                catch(Exception)
+                {
+
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.ExitThread();
+        }
+    }
+}
---
+++ Client/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Socket T;
+        string User;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string IP = textBox1.Text;
+            int Port = int.Parse(textBox2.Text);
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(IP), Port);
+            T = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            User = textBox3.Text;
+            try
+            {
+                T.Connect(EP);
+                Send("0" + User);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("無法連上伺服器！");
+                return;
+            }
+            button1.Enabled = false;
+        }
+
+        private void Send(string Str)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str);
+            T.Send(B, 0, B.Length, SocketFlags.None);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (button1.Enabled == false)
+            {
+                Send("9" + User);
+                T.Close();
+            }
+        }
+    }
+}
